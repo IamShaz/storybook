d20a436d17640b487bddd8faabb04c9f188dea4e
Shaz <iam.shaz@hotmail.com>
2019-04-24 10:19:33 -0400

    add button animations

---
+++ src/components/intro.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './intro.css'
+
+class Introduction extends React.Component {
+    render() {
+        return (<div>
+            <h1>Welcome to my Storybook</h1>
+        </div>);
+    }
+}
+
+export default Introduction;
